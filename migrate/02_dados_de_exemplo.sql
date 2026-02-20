a0a7490aa60312a3e5a20b9369fb972ad75eff72
Pietro <pietro.g.souza@aluno.senai.br>
2026-02-20 17:26:21 -0300

    alterando alguns dados da tabela

--- migrate/02_dados_de_exemplo.sql
+++ migrate/02_dados_de_exemplo.sql
@@ -15,7 +15,7 @@ INSERT INTO `soundkeep`.`musica`
 `nome_genero`)
 VALUES
 ("Seu Jorge",
-"3:38",
+"00:03:38",
 "Amiga da minha mulher",
 "https://upload.wikimedia.org/wikipedia/pt/6/6d/Capa_de_M%C3%BAsicas_para_Churrasco%2C_Vol._1.jpg",
 "Pagode");
@@ -30,7 +30,7 @@ INSERT INTO `soundkeep`.`musica`
 `nome_genero`)
 VALUES
 ("System of a down",
-"3:30",
+"00:03:30",
 "Chop Suey",
 "https://i.scdn.co/image/ab67616d00001e0207bc7d2a745636c356b4d0aa",
 "Rock");
